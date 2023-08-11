08d3d30c0fa6b090bbcb4dff683f172ddba16224
yomarajazmin <yomarajazmin@gmail.com>
2023-08-10 23:06:09 -0400

    Adding day 17 solution

---
+++ src/main/java/org/solutions/Day17_Exceptions.java
@@ -0,0 +1,30 @@
+package org.solutions;
+
+import java.util.Scanner;
+
+public class Day17_Exceptions {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine().trim());
+        Calculator calculator = new Calculator();
+        while (n > 0) {
+            try {
+                String[] numbers = scanner.nextLine().trim().split(" ");
+                System.out.println(calculator.power(Integer.parseInt(numbers[0]), Integer.parseInt(numbers[1])));
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+            n--;
+        }
+        scanner.close();
+    }
+}
+
+class Calculator {
+
+    public Integer power(Integer n, Integer p) throws Exception {
+        if (n < 0 || p < 0)
+            throw new Exception("n and p should be non-negative");
+        return (int) (Math.pow(n, p));
+    }
+}
